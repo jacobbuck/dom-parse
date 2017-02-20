daf6688f5554e37bc570dffae70cbd0f448b4bf2
Jacob Buck <buck.jacob@gmail.com>
2017-02-20 14:07:48 +1300

    Initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+'use strict';
+
+module.exports = function domify(markup) {
+	var doc;
+
+	if (typeof markup !== 'string') {
+		return false;
+	}
+
+	if (markup.toLowerCase().indexOf('<!doctype') > -1) {
+		markup = '<!doctype html>\n<html><body>'+markup+'</body></html>';
+	}
+
+	try {
+		doc = new DOMParser().parseFromString(markup, 'text/html');
+	} catch (e) {}
+
+	if (!doc) {
+		doc = document.implementation.createHTMLDocument('');
+		doc.open();
+		doc.write(markup);
+		doc.close();
+	}
+
+	if (!doc) {
+		return false;
+	}
+
+	return doc.body.childNodes;
+};
