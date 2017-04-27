1e7cbbd7133053681f124ba45b8f175afd054e74
Jacob Buck <buck.jacob@gmail.com>
2017-04-27 23:45:18 +1200

    Add options parameter, and strict option. Add html parsing when strict: false. Update readme

--- index.js
+++ index.js
@@ -1,28 +1,37 @@
 'use strict';
 
-module.exports = function domParse(markup) {
+module.exports = function domParse(markup, options) {
+  options = options || {};
+
   if (typeof markup !== 'string') return false;
 
   var doc;
+  var strict = options.strict !== false;
+  var mime = strict ? 'application/xhtml+xml' : 'text/html';
   var wrappedMarkup = '<!DOCTYPE html>\n<html><body>'+markup+'</body></html>';
 
   try {
-    doc = new DOMParser().parseFromString(wrappedMarkup, 'application/xhtml+xml');
+    doc = new DOMParser().parseFromString(wrappedMarkup, mime);
   } catch (e) {}
 
   if (doc && doc.getElementsByTagName('parsererror').length) return false;
 
-  if (!doc) {
+  if (strict) {
     doc = document.implementation.createDocument(
       'http://www.w3.org/1999/xhtml',
       'html',
       document.implementation.createDocumentType('html', null, null)
     );
     doc.documentElement.appendChild(doc.createElement('body'));
-    doc.body.innerHTML = markup;
+  } else {
+    doc = document.implementation.createHTMLDocument('');
   }
 
-  if (!doc) return false;
+  try {
+    doc.body.innerHTML = markup;
+  } catch (e) {
+    return false;
+  }
 
   return doc.getElementsByTagName('body')[0].childNodes;
 };
--- readme.md
+++ readme.md
@@ -6,11 +6,13 @@ Safely parse an HTML string into DOM using `DOMParser` or falling back to `DOMIm
 
 ```js
 domParse(markup)
+domParse(markup, options)
 ```
 
 ### Parameters
 
 - `markup` string of HTML you'd like to parse
+- `options` object of options
 
 ### Return value
 
@@ -23,4 +25,26 @@ import domParse from 'dom-parse';
 
 domParse('Some <em>awesome</em> markup <img src="explosion.gif" />');
 // returns [#text, <em>, #text, <img>]
+```
+
+## Strict Mode
+
+By default dom-parse is in strict mode, which will parse the markup as XHTML/XML, requiring all tags to be closed:
+
+```js
+domParse('<img src="dealwithit.gif">');
+// returns false
+
+domParse('<p>Some lazy markup');
+// returns false
+```
+
+You can disable strict mode optionally:
+
+```js
+domParse('<img src="dealwithit.gif">', { strict: false });
+// returns [<img>]
+
+domParse('<p>Some lazy markup');
+// returns [p]
 ```
