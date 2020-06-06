605d36edec66e06138b38fa3d84155b4566e38e7
Jacob Buck <buck.jacob@gmail.com>
2020-06-06 17:10:45 +1200

    Remove type checks

--- src/server.js
+++ src/server.js
@@ -1,11 +1,5 @@
 import { JSDOM } from 'jsdom';
 
-const parseDom = (markup) => {
-  if (typeof markup !== 'string') {
-    throw new TypeError('Expected parameter "markup" to be a string.');
-  }
-
-  return JSDOM.fragment(markup).childNodes;
-};
+const parseDom = (markup) => JSDOM.fragment(markup).childNodes;
 
 export default parseDom;
