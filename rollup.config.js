5e72e286e1639c7b63446b01ce1f1f7fcb01ad16
Jacob Buck <buck.jacob@gmail.com>
2020-06-06 15:49:15 +1200

    Update rollup.config.js
    Output esmodule

--- rollup.config.js
+++ rollup.config.js
@@ -3,13 +3,18 @@ import babel from 'rollup-plugin-babel';
 export default [
   {
     input: 'src/browser.js',
-    output: { file: 'lib/browser.js', format: 'cjs' },
-    external: ['css-to-style', 'html-void-elements', 'react'],
+    output: {
+      file: 'lib/browser.js',
+      format: 'es',
+    },
     plugins: [babel()],
   },
   {
     input: 'src/server.js',
-    output: { file: 'lib/server.js', format: 'cjs' },
+    output: {
+      file: 'lib/server.js',
+      format: 'es',
+    },
     external: ['jsdom'],
     plugins: [babel()],
   },
