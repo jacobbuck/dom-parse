4cb1df91982e57da976acd8eba11d030a1c2e491
Jacob Buck <buck.jacob@gmail.com>
2020-06-06 16:05:53 +1200

    Add type check to server.js

--- src/server.js
+++ src/server.js
@@ -1,5 +1,11 @@
 import { JSDOM } from 'jsdom';
 
-const parseDom = (markup) => JSDOM.fragment(markup).childNodes;
+const parseDom = (markup) => {
+  if (typeof markup !== 'string') {
+    throw new TypeError('Expected parameter "markup" to be a string.');
+  }
+
+  return JSDOM.fragment(markup).childNodes;
+};
 
 export default parseDom;
