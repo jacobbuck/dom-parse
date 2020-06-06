201e07317ec4e9100b8776697241207f048483f7
Jacob Buck <buck.jacob@gmail.com>
2020-06-06 17:11:02 +1200

    Add tests

---
+++ src/__tests__/browser.test.js
@@ -0,0 +1,17 @@
+import parseDom from '../browser';
+
+test('parses HTML string to DOM Node tree', () => {
+  expect(parseDom('Some <em>awesome</em> markup <img src="explosion.gif">'))
+    .toMatchInlineSnapshot(`
+    NodeList [
+      Some ,
+      <em>
+        awesome
+      </em>,
+       markup ,
+      <img
+        src="explosion.gif"
+      />,
+    ]
+  `);
+});
---
+++ src/__tests__/server.test.js
@@ -0,0 +1,17 @@
+import parseDom from '../server';
+
+test('parses HTML string to DOM Node tree', () => {
+  expect(parseDom('Some <em>awesome</em> markup <img src="explosion.gif">'))
+    .toMatchInlineSnapshot(`
+    NodeList [
+      Some ,
+      <em>
+        awesome
+      </em>,
+       markup ,
+      <img
+        src="explosion.gif"
+      />,
+    ]
+  `);
+});
