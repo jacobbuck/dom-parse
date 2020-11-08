f2473ddcb519c2ab34fd18627c2ddaeaebf840e1
Jacob Buck <buck.jacob@gmail.com>
2020-11-08 02:09:19 +0000

    Set exports option on cjs outputs in rollup.config.js to supress warning

--- rollup.config.js
+++ rollup.config.js
@@ -4,16 +4,34 @@ export default [
   {
     input: 'src/browser.js',
     output: [
-      { file: 'lib/browser.cjs.js', format: 'cjs', sourcemap: true },
-      { file: 'lib/browser.esm.js', format: 'esm', sourcemap: true },
+      {
+        exports: 'default',
+        file: 'lib/browser.cjs.js',
+        format: 'cjs',
+        sourcemap: true,
+      },
+      {
+        file: 'lib/browser.esm.js',
+        format: 'esm',
+        sourcemap: true,
+      },
     ],
     plugins: [babel({ babelHelpers: 'bundled' })],
   },
   {
     input: 'src/server.js',
     output: [
-      { file: 'lib/server.cjs.js', format: 'cjs', sourcemap: true },
-      { file: 'lib/server.esm.js', format: 'esm', sourcemap: true },
+      {
+        exports: 'default',
+        file: 'lib/server.cjs.js',
+        format: 'cjs',
+        sourcemap: true,
+      },
+      {
+        file: 'lib/server.esm.js',
+        format: 'esm',
+        sourcemap: true,
+      },
     ],
     external: ['jsdom'],
     plugins: [babel({ babelHelpers: 'bundled' })],
