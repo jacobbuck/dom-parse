b7a9cb596d6b5179b61fd44d3fbe84676412cf8a
Jacob Buck <buck.jacob@gmail.com>
2020-11-08 02:04:49 +0000

    Generate source maps with rollup

--- rollup.config.js
+++ rollup.config.js
@@ -4,16 +4,16 @@ export default [
   {
     input: 'src/browser.js',
     output: [
-      { file: 'lib/browser.cjs.js', format: 'cjs' },
-      { file: 'lib/browser.esm.js', format: 'esm' },
+      { file: 'lib/browser.cjs.js', format: 'cjs', sourcemap: true },
+      { file: 'lib/browser.esm.js', format: 'esm', sourcemap: true },
     ],
     plugins: [babel({ babelHelpers: 'bundled' })],
   },
   {
     input: 'src/server.js',
     output: [
-      { file: 'lib/server.cjs.js', format: 'cjs' },
-      { file: 'lib/server.esm.js', format: 'esm' },
+      { file: 'lib/server.cjs.js', format: 'cjs', sourcemap: true },
+      { file: 'lib/server.esm.js', format: 'esm', sourcemap: true },
     ],
     external: ['jsdom'],
     plugins: [babel({ babelHelpers: 'bundled' })],
