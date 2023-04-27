9343791bf4ff005178d27e9219d9bdece3523ec2
Jacob Buck <buck.jacob@gmail.com>
2023-04-27 23:55:20 +0000

    Add jsdoc comments

--- src/browser.js
+++ src/browser.js
@@ -1,3 +1,8 @@
+/**
+ * Parse a string of HTML or XML to a DOM tree.
+ * @param {string} [html] String containing HTML or XML to be parsed.
+ * @returns {DocumentFragment} Object containing Node structure of HTML/XML parsed.
+ */
 export const parse = (html) => {
   const element = document.createElement('template');
   if (html !== null && html !== undefined) {
--- src/node.js
+++ src/node.js
@@ -2,4 +2,9 @@ import { Window, XMLParser } from 'happy-dom';
 
 const { document } = new Window();
 
+/**
+ * Parse a string of HTML or XML to a DOM tree.
+ * @param {string} [html] String containing HTML or XML to be parsed.
+ * @returns {DocumentFragment} Object containing Node structure of HTML/XML parsed.
+ */
 export const parse = (html) => XMLParser.parse(document, html);
